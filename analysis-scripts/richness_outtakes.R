57408c4ffa94699fa2ce7269ceafe04420406570
bsevansunc <bsevans.unc@gmail.com>
2014-10-29 16:05:36 -0400

    added more richness outtakes ... adjusted r2 and expected values

--- analysis-scripts/richness_outtakes.R
+++ analysis-scripts/richness_outtakes.R
@@ -32,3 +32,22 @@ plot(lc$can, sr)
 
 plot(lc$imp, e1)
 plot(lc$can, e1)
+
+# Function that takes the model average frame, imp and canopy and
+# calculates the expected values (global model):
+
+expected.values = function(response, imp, can){
+  mod = summary.outs(response)[[2]][,2]
+  mod[1]+mod[2]*imp+mod[3]*can+mod[4]*imp*can+mod[5]*(imp^2)+mod[6]*(can^2)
+}
+
+# Function to determine the adjusted R2 averaged across the model:
+
+adj.r2 = function(response, imp, can){
+  aic.table = summary.outs(sr)[[1]]
+  observed = response
+  expected = expected.values(sr, imp, can)
+  r2 = cor(observed, expected)^2
+  k.ave = sum(aic.table$k*aic.table$w)
+  ((length(response)-1)*r2-k.ave)/(length(response)-1-k.ave)
+}
