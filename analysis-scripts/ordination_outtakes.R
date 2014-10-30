7e57fca441236a7f1ba94e3cdfe3d577c882b704
bsevansunc <bsevans.unc@gmail.com>
2014-10-30 10:52:20 -0400

    added script to the ordination outtakes file

--- analysis-scripts/ordination_outtakes.R
+++ analysis-scripts/ordination_outtakes.R
@@ -273,3 +273,37 @@ ord2.fit = envfit(ord2 ~ imp + can, data=lc, perm=1000)
 plot(ord2.fit, col =1)
 text(ord2.troph, display = "spec", cex=0.7, col="darkgreen")
 text(ord2.nest, display = "spec", cex=0.7, col="red")
+
+
+# Create a dataframe of expected richness values by potentential combinations of
+# impervious and canopy:
+
+expected.frame = function(response,imp, can){
+  # Create modeled values of imp and can:
+  imp = imp
+  can = can
+  # Make grid:
+  grid = mesh(imp,can)
+  # Exclude values for which the percent combination exceeds 100
+  g1 = grid$x + grid$y
+  g1 = ifelse(g1>100, NA, 1)
+  # Project values:
+  z = with(grid, expected.values(sr,imp,can))
+  # Limit to potential combinations of imp and can:
+  z = z*g1
+  # Create a dataframe for output
+  out.df = melt(z)
+  out.df = na.omit(out.df)
+  names(out.df) = c('imp','can','richness')
+  return(out.df)
+}
+
+expected.frame(sr, lc$imp,lc$can)
+
+R2 <- cor(sr,e1)^2
+
+plot(lc$imp, sr)
+plot(lc$can, sr)
+
+plot(lc$imp, e1)
+plot(lc$can, e1)
