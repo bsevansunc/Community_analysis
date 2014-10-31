7fb0b32723e626259a77535542969a670981cfa4
bsevansunc <bsevans.unc@gmail.com>
2014-10-31 13:57:04 -0400

    added preparation of abundance file for analysis (pc.abund)

--- analysis-scripts/pc_prep2.R
+++ analysis-scripts/pc_prep2.R
@@ -161,3 +161,98 @@ pc = pc[,-53]
 
   trophic.logtransformed = guild.log.transform(trophic)
   nest.logtransformed = guild.log.transform(nest)
+
+#--------------------------------------------------------------------------------*
+# ---- PREPARE SPECIES POINT COUNT DATA FOR ABUNDANCE ANALYSIS ----
+#================================================================================*
+
+pc = read.csv('derived-data/pc_10_14.csv')
+g = read.csv('derived-data/guilds.csv')
+lc = read.csv('derived-data/pts_lc100.csv')
+
+#--------------------------------------------------------------------------------*
+# ---- ABUNDANCE ACROSS SPECIES ----
+#================================================================================*
+
+# Subset pc frame into site, year, species, and sum of detections across 
+# distance classes:
+
+pc2 = pc[,c(1,2,6)]
+
+pc2$counts = rowSums(pc[7:11])
+
+# Because multiple time intervals are present (and removal method is used), sum
+# by year:
+
+nyrs = ddply(pc2, .(site), summarize, c = length(unique(year)))
+t.count = ddply(pc2, .(site), summarize, c = sum(counts))
+
+pc3 = merge(nyrs,t.count, by = 'site')
+pc3$t.a = pc3[,3]/pc3[,2]
+
+pc3 = pc3[,-3]
+
+# merge with lc frame:
+
+pc.t.abund = merge(pc3, lc, all = F)
+
+pc.t.abund = pc.t.abund[,-c(4,5)]
+
+pc.abund = pc.t.abund[,c(1:2,4:5,3)]
+
+names(pc.abund)[2] = 'nyrs'
+
+#--------------------------------------------------------------------------------*
+# ---- Relative abundance by guild ----
+#================================================================================*
+
+# Get data:
+
+g$species = tolower(g$species)
+
+# Merge count and guild data:
+
+pcg = merge(pc2,g, all = F)
+
+# Remove Bert Drake:
+
+pcg = pcg[pcg$site!='DRAKBERMD1',]
+
+# Combine feeding and trophic guilds:
+
+pcg$trophic = paste(pcg$foraging, pcg$trophic, sep='-')
+
+# Calculate the sum by trophic and nest guild
+
+trophic.count = ddply(pcg, .(site, trophic), summarize, c = sum(counts))
+
+nest.count = ddply(pcg, .(site, nest), summarize, c = sum(counts))
+
+# Convert to wide format:
+
+troph.melt = melt(trophic.count, id = c('site','trophic'))
+nest.melt = melt(nest.count, id = c('site','nest'))
+
+troph.cast = cast(troph.melt, site ~ trophic ~ variable,sum)
+nest.cast = cast(nest.melt, site ~ nest ~ variable,sum)
+
+troph.df = data.frame(troph.cast)
+nest.df = data.frame(nest.cast)
+
+troph.df$site = row.names(troph.df)
+nest.df$site = row.names(nest.df)
+
+# Merge with the count frame:
+
+pc.abund2 = merge(pc.abund, troph.df)
+pc.abund2 = merge(pc.abund2, nest.df)
+
+pc.abund = pc.abund2
+
+# Calculate the abundance scaled by years and total abundance for
+# each row of the life history guilds:
+
+for (i in 6:length(names(pc.abund))){
+  pc.abund[,i] = pc.abund[,i]/pc.abund[,2]/pc.abund[,5]
+}
+
