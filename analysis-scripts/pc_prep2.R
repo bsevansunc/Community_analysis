53c9b33b061f972517473fefcd2cf6db77c8caac
bsevansunc <bsevans.unc@gmail.com>
2014-11-12 18:27:45 -0500

    updated the process for creating relative abundance dataframes

--- analysis-scripts/pc_prep2.R
+++ analysis-scripts/pc_prep2.R
@@ -57,7 +57,13 @@ t4 = data.frame(t3)
 
 colnames(t4) = gsub('.counts','',colnames(t4))
 
-counts = t4
+counts.sp = t4
+
+#--------------------------------------------------------------------------------*
+# ---- Total abundance (mean counts across species) ----
+#--------------------------------------------------------------------------------*
+
+counts.total = data.frame(count = rowSums(counts.sp))
 
 #--------------------------------------------------------------------------------*
 # ---- Log-transformed count data ----
@@ -107,13 +113,13 @@ nest.counts = guild.shape('nest')
 trophic.counts = guild.shape('trophic')
 foraging_trophic.counts = guild.shape('foraging_trophic')
 
-###############################################################################################################################################
-# WORKS TO THIS POINT
-###############################################################################################################################################
+#--------------------------------------------------------------------------------*
+# Log-transformed count data
+#--------------------------------------------------------------------------------*
 
   guild.log.transform = function(life.history){
     # Get data
-     pc2 = prep.guild(life.history)
+     pc2 = life.history
     # Convert to log-transformed counts:
       for(i in 1:dim(pc2)[2]){
         pc2[,i] = log(1+pc2[,i])
@@ -121,116 +127,23 @@ foraging_trophic.counts = guild.shape('foraging_trophic')
     return(pc2)
     }
 
-# Return raw point count abundances by guild:
-
-  pc.trophic = prep.guild(trophic)
-  pc.nest = prep.guild(nest)
-
-# Return log-transformed abundances by guild:
-
-  trophic.logtransformed = guild.log.transform(trophic)
-  nest.logtransformed = guild.log.transform(nest)
-
-#--------------------------------------------------------------------------------*
-# ---- PREPARE SPECIES POINT COUNT DATA FOR ABUNDANCE ANALYSIS ----
-#================================================================================*
-
-pc = read.csv('derived-data/pc_10_14.csv')
-g = read.csv('derived-data/guilds.csv')
-lc = read.csv('derived-data/pts_lc100.csv')
-
-#--------------------------------------------------------------------------------*
-# ---- ABUNDANCE ACROSS SPECIES ----
-#================================================================================*
-
-# Subset pc frame into site, year, species, and sum of detections across 
-# distance classes:
-
-pc2 = pc[,c(1,2,6)]
-
-pc2$counts = rowSums(pc[7:11])
-
-# Because multiple time intervals are present (and removal method is used), sum
-# by year:
-
-nyrs = ddply(pc2, .(site), summarize, c = length(unique(year)))
-t.count = ddply(pc2, .(site), summarize, c = sum(counts))
-
-pc3 = merge(nyrs,t.count, by = 'site')
-pc3$t.a = pc3[,3]/pc3[,2]
-
-pc3 = pc3[,-3]
-
-# merge with lc frame:
-
-pc.t.abund = merge(pc3, lc, all = F)
-
-pc.t.abund = pc.t.abund[,-c(4,5)]
-
-pc.abund = pc.t.abund[,c(1:2,4:5,3)]
-
-names(pc.abund)[2] = 'nyrs'
+nest.counts.lt = guild.log.transform(nest.counts)
+trophic.counts.lt = guild.log.transform(trophic.counts)
+foraging_trophic.counts.lt = guild.log.transform(foraging_trophic.counts)
 
 #--------------------------------------------------------------------------------*
 # ---- Relative abundance by guild ----
-#================================================================================*
+#--------------------------------------------------------------------------------*
 
-# Get data:
+guild.rel.abund = function(guild.df){
+  rel.g.abund = list()
+    for (i in 1:ncol(nest.counts)){
+      rel.g.abund[[i]] = guild.df[i]/(rowSums(guild.df)-guild.df[i])
+    }
+    df = do.call('cbind', rel.g.abund)
+    return(df)
+}
 
-g$species = tolower(g$species)
-
-# Merge count and guild data:
-
-pcg = merge(pc2,g, all = F)
-
-# Remove Bert Drake:
-
-pcg = pcg[pcg$site!='DRAKBERMD1',]
-
-# Combine feeding and trophic guilds:
-
-pcg$trophic = paste(pcg$foraging, pcg$trophic, sep='-')
-
-# Calculate the sum by trophic and nest guild
-
-trophic.count = ddply(pcg, .(site, trophic), summarize, c = sum(counts))
-
-nest.count = ddply(pcg, .(site, nest), summarize, c = sum(counts))
-
-# Convert to wide format:
-
-troph.melt = melt(trophic.count, id = c('site','trophic'))
-nest.melt = melt(nest.count, id = c('site','nest'))
-
-troph.cast = cast(troph.melt, site ~ trophic ~ variable,sum)
-nest.cast = cast(nest.melt, site ~ nest ~ variable,sum)
-
-troph.df = data.frame(troph.cast)
-nest.df = data.frame(nest.cast)
-
-troph.df$site = row.names(troph.df)
-nest.df$site = row.names(nest.df)
-
-# Merge with the count frame:
-
-pc.abund2 = merge(pc.abund, troph.df)
-pc.abund2 = merge(pc.abund2, nest.df)
-
-pc.abund = pc.abund2
-
-# Calculate the abundance scaled by years
-# 
-# for (i in 6:length(names(pc.abund))){
-#   pc.abund[,i] = pc.abund[,i]/pc.abund[,2]
-# }
-
-# Make separate frames for trophic and nesting guilds:
-
-troph.ab = pc.abund[,c(1,6:18)]
-
-troph.ab$t = rowSums(troph.ab[,2:14])
-
-nest.ab = pc.abund[,c(1,19:24)]
-
-nest.ab$t = rowSums(nest.ab[,2:7])
-
+nest.ra = guild.rel.abund(nest.counts)
+trophic.ra = guild.rel.abund(trophic.counts)
+foraging_trophic.ra = guild.rel.abund(foraging_trophic.counts)
