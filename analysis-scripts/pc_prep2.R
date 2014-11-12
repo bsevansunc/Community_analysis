ae70125df9aec7707b84fe8e20e988477929d292
bsevansunc <bsevans.unc@gmail.com>
2014-11-12 18:04:12 -0500

    simplified the script for the preparation of count data

--- analysis-scripts/pc_prep2.R
+++ analysis-scripts/pc_prep2.R
@@ -37,122 +37,79 @@ lc = merge(lc, pc.site, all = F)
 # ---- PREPARE SPECIES POINT COUNT DATA FOR ANALYSIS ----
 #================================================================================*
 
-# For this run, I will use the max count per location across distance classes:
+# For this run, I will use the mean count per location across distance classes:
 
-pc$counts = pc[,7]+pc[,8]+pc[,9]+pc[,10]+pc[,11]
+pc$counts = rowSums(pc[,7:11])
 
 # Prepare count frame
 
-pc = pc[,-c(3:5,7:15)]
+counts1 =  pc[,-c(3:5,7:15)]
 
-t = melt(pc, id = c('site','year','species'))
+t1 = melt(counts1, id = c('site','year','species'))
 
-t2 = cast(t, site ~ species ~ variable,mean)
+t2 = data.frame(cast(t1, site ~ species ~ variable, mean))
 
-t3 = data.frame(t2)
+t3 = as.matrix(t2)
 
-t3m = as.matrix(t3)
+t3[is.na(t3)]<-0
 
-t3m[t3m == -Inf]<-0
-
-t4 = data.frame(t3m)
+t4 = data.frame(t3)
 
 colnames(t4) = gsub('.counts','',colnames(t4))
 
-# One of the sites has never had a count, remove it:
-
-t4$sums = rowSums(t4)
-
-t4 = t4[t4$sums!=0,-56]
-
-# Ensure all lc and pc files are covered (and vice-versa):
-
-lc = unique(lc)
-
-lc.sites = data.frame(lc[,1])
-colnames(lc.sites) = 'site'
-
-t4$site = row.names(t4)
-
-t5 = merge(t4, lc.sites, all = F)
-
-pc.sites = data.frame(t5$site)
-names(pc.sites) = 'site'
-
-pc = t5[,-1]
-
-lc1 = merge(lc, pc.sites, all = F, incomparables = NA)
-
-lc = lc1
+counts = t4
 
 #--------------------------------------------------------------------------------*
-# ---- Log-transform count data ----
+# ---- Log-transformed count data ----
 #--------------------------------------------------------------------------------*
 
-pc2 = pc[,-53]
+counts.lt = counts
 
-for(i in 1:dim(pc2)[2]){
-  pc2[,i] = log(1+pc2[,i])
+for(i in 1:dim(counts)[2]){
+  counts.lt[,i] = log(1+counts.lt[,i])
 }
-
-pc.logtransformed = pc2
-
-pc = pc[,-53]
 
 #--------------------------------------------------------------------------------*
 # ---- Counts by guild ----
 #================================================================================*
 
-# Get data:
+# Will use t1 (molten data frame) as the starting point, merging with
+# trophic, nest, and foraging-trophic guilds. First, I will need to sum the
+# counts across species within a guild for a given year, then I will need to
+# take the average values across years.
 
-  pc1 = read.csv('derived-data/pc_10_14.csv')
+# Create a new row of the guild frame htat combines foraging and trophic
+# guilds:
+
+  g$foraging_trophic = paste(g$foraging, g$trophic, sep ='_')
+
+# Merge the frames (also removes species not in both lists):
+
   g$species = tolower(g$species)
   
-  pcg = merge(pc1,g, all = F)
+  pcg = merge(t1,g, all = F)
 
-# For this run, I will use the max count per location across distance classes:
+# Function to create the count frame for a given guild:
 
-  pcg$counts = pcg[,7]+pcg[,8]+pcg[,9]+pcg[,10]+pcg[,11]
+  guild.shape = function(guild){
+    t1a = aggregate(pcg$value, by = list(pcg$site,pcg$year,pcg[,guild]),sum)
+      names(t1a) = c('site','year','guild','count')
+    t1 = melt(t1a, id = c('site','year','guild'))
+    t2 = data.frame(cast(t1, site ~ guild ~ variable, mean))
+    t3 = as.matrix(t2)
+    t3[is.na(t3)]<-0
+    t4 = data.frame(t3)
+    colnames(t4) = gsub('.count','',colnames(t4))
+    return(t4)
+    }
 
-# Remove Bert Drake:
+nest.counts = guild.shape('nest')
+trophic.counts = guild.shape('trophic')
+foraging_trophic.counts = guild.shape('foraging_trophic')
 
-  pcg = pcg[pcg$site!='DRAKBERMD1',]
-
-# Separate by life history trait
-
-  pcg.troph = pcg
-  pcg.nest = pcg
-
-# Combine feeding and trophic guilds:
-
-  pcg$trophic = paste(pcg.troph$foraging, pcg.troph$trophic, sep='-')
-
-# Aggregate count data by guild:
-
-  trophic = aggregate(pcg$counts,by = list(pcg$site,pcg$year,pcg$trophic),sum)
-    names(trophic) = c('site','year','guild','count')
-  nest = aggregate(pcg$counts,by = list(pcg$site,pcg$year,pcg$nest),sum)
-    names(nest) = c('site','year','guild','count')
-
-# Functions to prepare count frames for raw and log-transformed abundances:
-
-  prep.guild = function(life.history){
-    t = melt(life.history, id = c('site','year','guild'))
-    t2 = cast(t, site ~ guild,mean)
-    t3 = data.frame(t2)[,-1]
-    t3m = as.matrix(t3)
-    t3m[t3m == -Inf]<-0
-    t4 = data.frame(t3m)
-    t4$site = t2$site
-    lc = unique(lc)
-    lc.sites = data.frame(lc[,1])
-    colnames(lc.sites) = 'site'
-    t5 = merge(t4, lc.sites, all = F)
-    pc.sites = data.frame(t2$site)
-    names(pc.sites) = 'site'
-    pc = t5[,-1]
-    return(pc)
-  }
+###############################################################################################################################################
+# WORKS TO THIS POINT
+###############################################################################################################################################
 
   guild.log.transform = function(life.history){
     # Get data
