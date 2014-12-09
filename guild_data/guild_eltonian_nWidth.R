9a723cd0ef816549d04d8bf5b3fa3fcde8520899
= <bsevans.unc@gmail.com>
2014-12-09 15:21:12 -0500

    fixed assignments in file, deleted lines

--- guild_data/guild_eltonian_nWidth.R
+++ guild_data/guild_eltonian_nWidth.R
@@ -14,25 +14,13 @@ ge = ge[ge$alpha!='rsha',]
 
 diet = data.frame(row.names = ge[,2], ge[,20:28])
 
-diet.div = diversity(diet)
-
-diet.even = diet.div/log(length(diet))
-
-diet$div = diet.div
-
-diet$even = diet.even
+dietEven = diversity(diet)/log(length(diet))
 
 # Foraging strata diversity:
 
 fs = data.frame(row.names = ge[,2], ge[,30:34])
 
-fs.div = diversity(fs)
-
-fs.even = fs.div/log(length(fs))
-
-fs$div = fs.div
-
-fs$even = fs.even
+fsEven = diversity(fs)/log(length(fs))
 
 # Nest height variation (alas, diversity or evenness not possible with the available data):
 
@@ -50,14 +38,13 @@ nestling_var = ge$Nestling_period_max - ge$Nestling_period_min
 
 clutch_var = ge$Clutch_max - ge$Clutch_min
 
-# Variation in broods per year:
+# Number of broods per year:
 
 brood_var = ge$Brood_max
 
 # Summarizing Eltonian niche width:
 
-enw.df = data.frame(sp = ge$alpha, dietEven = diet.even, 
-                    fsEven = fs.even, varHeight, 
+enw.df = data.frame(sp = ge$alpha, dietEven,fsEven, varHeight, 
                     inc_var, nestling_var, clutch_var, brood_var)
 
 # Because bhco is a nest parasite (with up to 40 broods per year, I will give an nHeight and breeding var of 0):
@@ -68,7 +55,7 @@ enw.df[enw.df$sp == 'bhco',c(4,8)]<- c(max(na.omit(enw.df$varHeight)),max(na.omi
 
 enw.df[enw.df$sp == 'howr',4]<-11.5
 
-# Convert to percentiles:
+# Function to convert to percentiles:
 
 pRank <- function(x) trunc(rank(x))/length(x)
 
@@ -85,34 +72,13 @@ enwP.df = enwP.df[,-c(5:8)]
 
 # Come up with a single index of niche width:
 
-enwP.df$enw1 = pRank(rowSums(enwP.df[,2:5]))
-
-enwP.df[order(enwP.df$enw1),]
-
-# Change to "1 - ..." so 0 represents the most generalist:
-
-for(i in 2:length(enwP.df)){
-  enwP.df[,i] = 1 - enwP.df[,i]
-}
-
-# Come up with a single index of niche width:
-
-enw.df$enw1 = rowSums(enw.df[,2:5])
+enwP.df$enw = pRank(rowSums(enwP.df[,2:5]))
 
 # Take a look!
 
-enw.df[order(enw.df$enw1),]
-
-# Scaled diet and foraging strata evenness (so 0's are generalist, recorded as: 1 -)
-
-diet.even = 1-pRank(diet.even)
-fs.even = 1-pRank(fs.even)
-
-nHeight_var = 1- pRank(varHeight)
-
-
+enwP.df[order(enwP.df$enw),]
 
 # Write file:
 
-write.csv(enw.df, 'eltonian_niche_width.csv', row.names = F)
+write.csv(enwP.df, 'eltonian_niche_width.csv', row.names = F)
 
