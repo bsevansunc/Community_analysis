552ad123412249512c4b9a1b73e47c32ee47374b
= <bsevans.unc@gmail.com>
2014-12-09 15:50:07 -0500

    simplified breeding niche

--- guild_data/guild_eltonian_nWidth.R
+++ guild_data/guild_eltonian_nWidth.R
@@ -2,6 +2,10 @@ library(vegan)
 
 ge = read.csv('guild_eltonian.csv')
 
+# Take away the red-shouldered hawk:
+
+ge = ge[ge$alpha!='rsha',]
+
 # Add min and max nest height data for the Carolina Wren (report from All About Birds):
 
 ge[ge$alpha == 'carw',4:5]<-c(0,2)
@@ -26,30 +30,33 @@ fsEven = diversity(fs)/log(length(fs))
 
 varHeight = ge$nest_max - ge$nest_min
 
-# Variation in incubation time:
+# Minimum incubation time:
 
-inc_var = ge$Inc_period_max - ge$Inc_period_min
+inc_min = ge$Inc_period_min
 
-# Variation in nestling time:
+# Minimum nestling time:
 
-nestling_var = ge$Nestling_period_max - ge$Nestling_period_min
+nestling_min = ge$Nestling_period_min
 
-# Variation in clutch size:
+# Maximum clutch size:
 
-clutch_var = ge$Clutch_max - ge$Clutch_min
+clutch_max = ge$Clutch_max
 
-# Number of broods per year:
+# Maximum number of broods per year:
 
-brood_var = ge$Brood_max
+brood_max = ge$Brood_max
+
+# Maximum number of offspring per year
+
+offspring_max = clutch_max*brood_max
 
 # Summarizing Eltonian niche width:
 
-enw.df = data.frame(sp = ge$alpha, dietEven,fsEven, varHeight, 
-                    inc_var, nestling_var, clutch_var, brood_var)
+enw.df = data.frame(sp = ge$alpha, dietEven,fsEven, varHeight, offspring_max)
 
 # Because bhco is a nest parasite (with up to 40 broods per year, I will give an nHeight and breeding var of 0):
 
-enw.df[enw.df$sp == 'bhco',c(4,8)]<- c(max(na.omit(enw.df$varHeight)),max(na.omit(enw.df$brood_var)))
+enw.df[enw.df$sp == 'bhco',c(4,5)]<- c(max(na.omit(enw.df$varHeight)),40)
 
 # Dobkon et al. 1995 says that House Wren exhibit 11.5 m range in nest height:
 
@@ -64,13 +71,7 @@ for (i in 2:length(enw.df)){
   enwP.df[,i] = 1-pRank(enw.df[,i])
 }
 
-enwP.df$breeding_var = pRank(rowSums(enwP.df[,5:8]))
-
-# Remove specific breeding info:
-
-enwP.df = enwP.df[,-c(5:8)]
-
-# Come up with a single index of niche width:
+# Come up with a single index of Eltonian niche width:
 
 enwP.df$enw = pRank(rowSums(enwP.df[,2:5]))
 
